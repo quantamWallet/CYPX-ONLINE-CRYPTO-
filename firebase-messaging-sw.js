df781683494f846333662ccd51d7f460450a4d67
CYPX ONLINE CRYPTO <quantumriokenya@gmail.com>
2026-06-07 11:39:24 +0300

    Update Firebase SDK versions and configuration

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,30 @@
 importScripts(
-'https://www.gstatic.com/firebasejs/10.12.3/firebase-app-compat.js'
+'https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js'
 );
 
 importScripts(
-'https://www.gstatic.com/firebasejs/10.12.3/firebase-messaging-compat.js'
+'https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js'
 );
 
 firebase.initializeApp({
   apiKey:
   "AIzaSyCCW4G9JlVu_xb0bGjlKmsi5mdqXSwvFak",
+
   authDomain:
   "quantum-rio.firebaseapp.com",
+
   databaseURL:
   "https://quantum-rio-default-rtdb.firebaseio.com",
+
   projectId:
   "quantum-rio",
+
   storageBucket:
-  "quantum-rio.appspot.com",
+  "quantum-rio.firebasestorage.app",
+
   messagingSenderId:
   "805433999377",
+
   appId:
   "1:805433999377:web:d0efa8c46ec4a0fa30f1b0"
 });
@@ -34,7 +40,11 @@ payload.notification.title,
 {
 body:
 payload.notification.body,
+
 icon:
+"cypx_icon_192x192.png",
+
+badge:
 "cypx_icon_192x192.png"
 }
 );
