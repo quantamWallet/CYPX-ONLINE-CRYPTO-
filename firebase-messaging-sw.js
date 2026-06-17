56d57a7926ad433c6b10e45c07f6a0b10d08a859
CYPX ONLINE CRYPTO <quantumriokenya@gmail.com>
2026-06-17 12:23:59 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,12 @@
 importScripts(
-'https://www.gstatic.com/firebasejs/11.10.0/firebase-app-compat.js'
+  'https://www.gstatic.com/firebasejs/11.10.0/firebase-app-compat.js'
 );
 
 importScripts(
-'https://www.gstatic.com/firebasejs/11.10.0/firebase-messaging-compat.js'
+  'https://www.gstatic.com/firebasejs/11.10.0/firebase-messaging-compat.js'
 );
 
 firebase.initializeApp({
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
   apiKey: "AIzaSyCCW4G9JlVu_xb0bGjlKmsi5mdqXSwvFak",
   authDomain: "quantum-rio.firebaseapp.com",
   databaseURL: "https://quantum-rio-default-rtdb.firebaseio.com",
@@ -17,6 +15,26 @@ const firebaseConfig = {
   messagingSenderId: "805433999377",
   appId: "1:805433999377:web:d0efa8c46ec4a0fa30f1b0",
   measurementId: "G-4RSQXZJX44"
-};
+});
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message',
+    payload
+  );
+
+  const notificationTitle =
+    payload.notification?.title || 'New Notification';
+
+  const notificationOptions = {
+    body: payload.notification?.body || '',
+    icon: '/icon-192.png'
+  };
+
+  self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
