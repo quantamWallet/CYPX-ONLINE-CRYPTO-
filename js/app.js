18a6f2ce071fffe02717774718a53bb5f6c9bb0c
CYPX ONLINE CRYPTO <quantumriokenya@gmail.com>
2026-06-17 11:49:11 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 import { app } from './firebase-config.js';
 
 console.log('Firebase initialized');
+import { requestNotificationPermission }
+from './notifications.js';
+
+requestNotificationPermission();
